2654eae43bbaf07b529058ba568c75db12839a33
Zhangzhongqiu <zhangzhongqiu1992@gmail.com>
2017-01-23 11:08:41 +0100

    the revised code. Registration from camera coordinate frame to robot coordinate frame

--- main2.m
+++ main2.m
@@ -1,6 +1,7 @@
 data=csvread('20161118_run_1.csv',21,1);
 [Q,tau,Tr2]=read.gettrans(data);
 [q_opt,q_test]=qget(Q);
+n=size(q_opt,1);
 tic
 %start to registration
 [C,theta_bc,d_bc]=reg.coordinate_trans(Q,Tr2);
@@ -9,4 +10,10 @@ t1=toc;
 x=opt.opt21(Q,tau,C);
 save('outputs.mat','x');
 t2=toc-t1;
+%calculate errors before and after calibration
+%[theta_ac,d_ac]=error.error_est(x,tau,q_opt,C);
 [theta_ac,d_ac]=error.error_est(x,tau,q_opt,C);
+
+theta=[theta_ac;theta_bc(1:n);]';
+d=[d_ac,d_bc(1:n)];
+[f1,f2]=plot.figplot(d,theta);
