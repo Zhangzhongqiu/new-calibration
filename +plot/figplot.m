5784deac1e0ebda16a5728eb54432de566e08d6c
Zhangzhongqiu <zhangzhongqiu1992@gmail.com>
2017-03-24 18:30:46 +0100

    Add files via upload

--- +plot/figplot.m
+++ +plot/figplot.m
@@ -20,22 +20,22 @@ s1=std(d);
 %legend
 ldg1=legend({['After calibration', sprintf('\n'), 'mean=',num2str(mean1(1)),'mm ', 'std=',num2str(s1(1)),'mm '...
     'max=',num2str(max1(1)),'mm'],['Before calibration (nominal model)',...
-    sprintf('\n'),'mean=',num2str(mean1(2)),'mm ','std=',num2str(s1(2)),'mm ','max=',num2str(max1(2)),'mm']},'FontSize',25);
+    sprintf('\n'),'mean=',num2str(mean1(2)),'mm ','std=',num2str(s1(2)),'mm ','max=',num2str(max1(2)),'mm']},'Location','southoutside','FontSize',25);
 ldg1.Box='off';
 %plot rotation error
-deg=pi/180;
-theta=theta/deg;
-edges2=(0:0.00018:0.009)/deg;
-ed2=(0:0.00018:0.00882)/deg;
+% deg=pi/180;
+% theta=theta/deg;
+edges2=0:0.00018:0.00918;
+ed2=0:0.00018:0.009;
 M(:,1)=histcounts(theta(:,1),edges2);
 M(:,2)=histcounts(theta(:,2),edges2);
 figure
 f2=bar(ed2,M);
 f2(1).FaceColor='b';
 f2(2).FaceColor='r';
-set(gca,'xlim',[0 0.01/deg],'YGrid','on','XTick',ed2,'XTickLabelRotation',45);
-set(gca,'XTickLabel',sprintf('%3.3f\n',ed2));
-xlabel('Rotation error [deg]');
+set(gca,'xlim',[0 0.01],'YGrid','on','XTick',ed2,'XTickLabelRotation',45);
+set(gca,'XTickLabel',sprintf('%3.2f\n',ed2*1000));
+xlabel('Rotation error [rad\times10^{-3}]');
 ylabel('Number of measurements');
 box off;
 mean2=mean(theta);
@@ -43,8 +43,8 @@ max2=max(theta);
 s2=std(theta);
 
 %legend
-ldg2=legend({['After calibration', sprintf('\n'), 'mean=',num2str(mean2(1)),'deg ', 'std=',num2str(s2(1)),'deg '...
-    'max=',num2str(max2(1)),'deg'],['Before calibration (nominal model)',...
-    sprintf('\n'),'mean=',num2str(mean2(2)),'deg ','std=',num2str(s2(2)),'deg ','max=',num2str(max2(2)),'deg']},'FontSize',25);
+ldg2=legend({['After calibration', sprintf('\n'), 'mean=',num2str(mean2(1)),'rad ', 'std=',num2str(s2(1)),'rad '...
+    'max=',num2str(max2(1)),'rad'],['Before calibration (nominal model)',...
+    sprintf('\n'),'mean=',num2str(mean2(2)),'rad ','std=',num2str(s2(2)),'rad ','max=',num2str(max2(2)),'rad']},'Location','southoutside','FontSize',25);
 ldg2.Box='off';
 end
